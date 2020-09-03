80ce74b2d176697e081b76f9db9e0047efdf08da
lichrok <s.dmitry.dev@gmail.com>
2020-09-03 15:06:26 +0300

    Updates

---
+++ precache-manifest.3bd5f8c80d2c901994aa7c5bd767cc29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7fc814bb30b76e20af4d7e9b35d52e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac736ea63cd77aea3520",
+    "url": "/static/js/2.e7dbf478.chunk.js"
+  },
+  {
+    "revision": "0c7f7415c0ec92798fd914561f3edc54",
+    "url": "/static/js/2.e7dbf478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf39634cf1ab88591d54",
+    "url": "/static/js/main.a93ada76.chunk.js"
+  },
+  {
+    "revision": "b0d964e8abd6f2227c85",
+    "url": "/static/js/runtime-main.2add13cc.js"
+  }
+]);
